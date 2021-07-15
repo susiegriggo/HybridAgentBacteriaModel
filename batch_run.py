969617b0d41186ed9604cb85b3aedee1aa056549
Susie Grigson <susie.grigson@flinders.edu.au>
2021-07-15 11:23:57 +0930

    updated to not runoff of the test files

--- batch_run.py
+++ batch_run.py
@@ -7,25 +7,19 @@ from multiprocessing import freeze_support
 from model import *
 
 fixed_params = {
-    "width": 0.1,
-    "height": 0.01,
-    "population":100
+    "width": 1,
+    "height": 0.1,
 }
 
-variable_params = {"name": ['batch1','batch2']}
+
+#vary a range of different parameters 
+#vary the variable name to introduce different replicates 
+variable_params = {"name": ['rep1', 'rep2', 'rep3'], "population": [10E-4, 10E-6, 10E-8], "c_star":[0.01, 0.1, 1], "beta_star": [10E-8,10E-6, 10E-4]}
 
 if __name__ == "__main__":
 
     freeze_support()
 
-    batch_run = BatchRunnerMP(Tube, nr_processes=None, fixed_parameters=fixed_params, variable_parameters= variable_params, iterations=1, max_steps=100, model_reporters= None, agent_reporters=None, display_progress= True)
+    batch_run = BatchRunnerMP(Tube, nr_processes=None, fixed_parameters=fixed_params, variable_parameters= variable_params, iterations=1, max_steps=100000, model_reporters= None, agent_reporters=None, display_progress= True)
 
     a = batch_run.run_all()
-
-    #get the data
-    #run_data = batch_run.get_model_vars_dataframe()
-    #print(run_data)
-    #print(np.min(run_data['Concentration'][0]))
-
-    #reintroduce periodically saving the concentration field and density
-    #save the final concentration field and density
