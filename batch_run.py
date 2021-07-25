16f1a5b2349327755278528056708003b4390155
Susie Grigson <susie.grigson@flinders.edu.au>
2021-07-25 23:34:14 +0930

    batch run

--- batch_run.py
+++ batch_run.py
@@ -7,19 +7,20 @@ from multiprocessing import freeze_support
 from model import *
 
 fixed_params = {
-    "width": 1,
-    "height": 0.1,
+    "width_": 0.1,
+    "height_": 0.01,
 }
 
-
-#vary a range of different parameters 
+ 
 #vary the variable name to introduce different replicates 
-variable_params = {"name": ['rep1', 'rep2', 'rep3'], "population": [10E-4, 10E-6, 10E-8], "c_star":[0.01, 0.1, 1], "beta_star": [10E-8,10E-6, 10E-4]}
+#variable_params = {"name": ['001cm_plate/001cm'], "population": [1000, 10000, 100000], "c_star":[0.1, 1], "beta_star": [10E-6,10E-7, 10E-8, 10E-9], "pattern":['tumble', 'reverse', 'flick'], 'dt':[0.01, 0.03, 0.05] , 'dx_':[0.0001] }
+variable_params = {"name": ['mini_tube_test/tube'], "population": [10000, 1000], "c_star":[ 1], "beta_star": [ 10E-9, 10E-10, 10E-11, 10E-12], "pattern":['tumble'], 'dt':[0.01] , 'dx_':[0.0001] }
+
 
 if __name__ == "__main__":
 
     freeze_support()
 
-    batch_run = BatchRunnerMP(Tube, nr_processes=None, fixed_parameters=fixed_params, variable_parameters= variable_params, iterations=1, max_steps=100000, model_reporters= None, agent_reporters=None, display_progress= True)
+    batch_run = BatchRunnerMP(Tube,  fixed_parameters=fixed_params, nr_processes = None, variable_parameters= variable_params, iterations=1, max_steps=300000, model_reporters= None, agent_reporters=None, display_progress= True)
 
     a = batch_run.run_all()
